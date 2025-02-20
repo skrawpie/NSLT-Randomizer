08b2c016096a596123b5ce439eed9b9ad4518c2f
skrawpie <21212370+skrawpie@users.noreply.github.com>
2025-02-20 13:51:42 -0500

    Delete levelrandomizer/obj directory

--- levelrandomizer/obj/Debug/net35/MyPluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace levelrandomizer
-{
-    public static class MyPluginInfo
-    {
-        public const string PLUGIN_GUID = "levelrandomizer";
-        public const string PLUGIN_NAME = "Level Randomizer";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
